e054cf48cf3601177f1bf56b0a99574da62a84d9
Adam Siekierski <adam.siekiera@outlook.com>
2020-06-23 10:55:13 +0200

    Get thoughts

--- src/providers/ThoughtsContext.js
+++ src/providers/ThoughtsContext.js
@@ -1,9 +1,22 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { db } from '../firebase';
 
 export const ThoughtsContext = React.createContext({ thoughts: [], addThought: () => {} });
 
 export function ThoughtsProvider({ children }) {
   const [thoughts, setThoughts] = useState([]);
+
+  useEffect(() => {
+    const unsubscribe = db.collection('thoughts').onSnapshot(({ docs }) => {
+      const newThoughts = [];
+      docs.map(doc => {
+        newThoughts.push(doc.data().content);
+      });
+      setThoughts(newThoughts);
+    });
+
+    return () => unsubscribe();
+  }, []);
 
   const addThought = thought => setThoughts(prevThoughts => [...prevThoughts, thought]);
 
